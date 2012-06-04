7358b0405f72ad10ee635cedc61e1829cb43a046
Minyoung Kim <cyzest@nate.com>
2012-06-05 02:45:25 +0900

    cluster setting

--- app.js
+++ app.js
@@ -1,51 +1,71 @@
 
 /**
- * Module dependencies.
+ * Cluster Setting
  */
 
-var express = require('express')
+var cluster = require('cluster');
+var http = require('http');
+var numCPUs = require('os').cpus().length;
+
+if(cluster.isMaster){
+  for(var i=0; i<numCPUs; i++){
+    cluster.fork();
+  }
+  cluster.on('death', function(worker){
+    console.log('worker '+worker.pid+' died');
+  });
+}else{
+
+  /**
+   * Module dependencies.
+   */
+
+  var express = require('express')
   , fs = require('fs')
   , routes = require('./routes');
 
-// DB Configuration
+  // DB Configuration
 
-require('./db-connect');
-var models_path = __dirname + '/models';
-var model_files = fs.readdirSync(models_path);
-model_files.forEach(function(file){
-	require(models_path+'/'+file);
-});
-MessageModel = mongoose.model('Message');
+  require('./db-connect');
+  var models_path = __dirname + '/models';
+  var model_files = fs.readdirSync(models_path);
+  model_files.forEach(function(file){
+    require(models_path+'/'+file);
+  });
+  MessageModel = mongoose.model('Message');
 
-// Create Server
+  // Create Server
 
-var app = module.exports = express.createServer();
+  var app = module.exports = express.createServer();
 
-// Configuration
+  // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+  app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+  });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
+  app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  });
 
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
+  app.configure('production', function(){
+    app.use(express.errorHandler()); 
+  });
 
-// Routes
+  // Routes
 
-app.get('/', routes.index);
+  app.get('/', routes.index);
 
-app.post('/add', routes.addMessage);
+  app.post('/add', routes.addMessage);
 
-var port = process.env.PORT || 10000
-app.listen(port);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+  var port = process.env.PORT || 10000
+  app.listen(port);
+  
+  //console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+}
